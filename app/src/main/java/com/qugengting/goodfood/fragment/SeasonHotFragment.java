bd4ba5dd64165ae83b8d3583323f4d32e3010205
xuruibin <xuruibin@nistone.cn>
2018-02-24 09:21:03 +0800

    添加当季最热页面内容；调整标题样式

--- app/src/main/java/com/qugengting/goodfood/fragment/SeasonHotFragment.java
+++ app/src/main/java/com/qugengting/goodfood/fragment/SeasonHotFragment.java
@@ -10,9 +10,27 @@ import android.view.ViewGroup;
 
 import com.common.library.fragment.BaseFragment;
 import com.common.library.pullableview.PullToRefreshLayout;
+import com.common.library.pullableview.PullableListView;
+import com.common.library.util.Utils;
 import com.qugengting.goodfood.R;
+import com.qugengting.goodfood.adapter.WeekSelectionAdapter;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by xuruibin on 2018/2/1.
@@ -20,20 +38,20 @@ import butterknife.Unbinder;
  */
 
 public class SeasonHotFragment extends BaseFragment {
-//    @BindView(R.id.lv_meishi)
-//    PullableListView listView;
-//    @BindView(R.id.refresh_view)
-//    PullToRefreshLayout ptrl;
-//    protected ProgressDialog dialog;
-//    private WeekSelectionAdapter adapter;
-//    private static final String TAG = MainActivity.class.getSimpleName();
-//    private List<String> list = new ArrayList<>();
-//    private static final String OK = "ok";
-//    private static final String ERROR = "error";
-//    private boolean isFirstIn = true;
-//    private static final String DEFAULT_MAIN_URL = "http://home.meishichina.com/show-top-type-recipe.html";
-//    private String mNextPageUrl = "";
-//    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36";
+    @BindView(R.id.lv_season_hot)
+    PullableListView listView;
+    @BindView(R.id.refresh_view_season_hot)
+    PullToRefreshLayout ptrl;
+    protected ProgressDialog dialog;
+    private WeekSelectionAdapter adapter;
+    private static final String TAG = WeekSelectionFragment.class.getSimpleName();
+    private List<String> list = new ArrayList<>();
+    private static final String OK = "ok";
+    private static final String ERROR = "error";
+    private boolean isFirstIn = true;
+    private static final String DEFAULT_MAIN_URL = "http://home.meishichina.com/show-top-type-recipe-order-quarter.html";
+    private String mNextPageUrl = "";
+    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36";
 
     private Unbinder unbinder;
     @Nullable
@@ -42,11 +60,11 @@ public class SeasonHotFragment extends BaseFragment {
         if (view == null) {
             view = inflater.inflate(R.layout.fragment_season_hot, container, false);
             unbinder = ButterKnife.bind(this, view);
-//            listView.setCanPullDown(false);
-//            adapter = new WeekSelectionAdapter(getActivity(), list);
-//            listView.setAdapter(adapter);
-//            ptrl.setOnRefreshListener(new MyRefreshListener());
-//            start(null);
+            listView.setCanPullDown(false);
+            adapter = new WeekSelectionAdapter(getActivity(), list);
+            listView.setAdapter(adapter);
+            ptrl.setOnRefreshListener(new SeasonHotFragment.MyRefreshListener());
+            start(null);
         }
         return view;
     }
@@ -55,105 +73,116 @@ public class SeasonHotFragment extends BaseFragment {
 
         @Override
         public void onRefresh(PullToRefreshLayout pullToRefreshLayout) {
+            //start(pullToRefreshLayout);
         }
 
         @Override
         public void onLoadMore(PullToRefreshLayout pullToRefreshLayout) {
-//            start(pullToRefreshLayout);
+            start(pullToRefreshLayout);
         }
     }
 
-//    public void start(final PullToRefreshLayout pullToRefreshLayout) {
-//        if (isFirstIn) {
-//            dialog = new ProgressDialog(getContext());
-//            dialog.setMessage("加载中...");
-//            dialog.show();
-//        }
-//        Observable.create(new Observable.OnSubscribe<String>() {
-//            @Override
-//            public void call(Subscriber<? super String> subscriber) {
-//                String result = getDatas();
-//                subscriber.onNext(result);
-//                subscriber.onCompleted();
-//            }
-//        }).subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(new Subscriber<String>() {
-//                    @Override
-//                    public void onCompleted() {
-//                        if (dialog != null && dialog.isShowing()) {
-//                            dialog.dismiss();
-//                        }
-//                        if (pullToRefreshLayout != null) {
-//                            pullToRefreshLayout.refreshFinish(PullToRefreshLayout.SUCCEED);
-//                        }
-//                    }
-//
-//                    @Override
-//                    public void onError(Throwable e) {
-//                        if (dialog != null && dialog.isShowing()) {
-//                            dialog.dismiss();
-//                        }
-//                        if (pullToRefreshLayout != null) {
-//                            pullToRefreshLayout.refreshFinish(PullToRefreshLayout.FAIL);
-//                        }
-//                        Utils.makeText(SeasonHotFragment.this.getActivity(), "网络错误");
-//                    }
-//
-//                    @Override
-//                    public void onNext(String s) {
-//                        adapter.notifyDataSetChanged();
-//                    }
-//                });
-//    }
-//    private String getDatas() {
-//        try {
-//            long a = System.currentTimeMillis();
-//            Connection connection;
-//            Document doc;
-//            if (isFirstIn) {
-//                connection = Jsoup.connect(DEFAULT_MAIN_URL);
-//            } else {
-//                connection = Jsoup.connect(mNextPageUrl);
-//            }
-//            connection.header("User-Agent", USER_AGENT);
-//            doc = connection.get();
-//            //“椒麻鸡”和它对应的图片都在<div class="pic">中
-//            Elements titleAndPics = doc.select("div.pic");
-//            //原料在<p class="subcontent">中
-//            Elements burdens = doc.select("p.subcontent");
-//            //所需链接在<div class="detail">中的<a>标签里面
-//            Elements detail = doc.select("div.detail");
-//            for (int i = 0; i < titleAndPics.size(); i++) {
-//                //使用Element.select(String selector)查找元素，使用Node.attr(String key)方法取得一个属性的值
-//                String title = titleAndPics.get(i).select("a").attr("title");
-//                String pic = titleAndPics.get(i).select("a").select("img").attr("data-src");
-//                String url = detail.get(i).select("a").first().attr("href");
-//                //对于一个元素中的文本，可以使用Element.text()方法
-//                String burden = burdens.get(i).text();
-//                Log.e(TAG, "tvTitle:" + title);
-//                Log.e(TAG, "pic:" + pic);
-//                Log.e(TAG, "url:" + url);
-//                Log.e(TAG, "burden:" + burden);
-//                list.add(title + "=====" + pic + "=====" + burden + "=====" + url);
-//            }
-//            Elements pageElements = doc.select("div.ui-page-inner");
-//            Element nowPageElement = pageElements.select("a.now_page").get(0);
-//            Element nextPageElement = nowPageElement.nextElementSibling();
-//            mNextPageUrl = nextPageElement.attr("href");
-//            isFirstIn = false;
-//            long b = System.currentTimeMillis();
-//            Log.e(TAG, "所用时间：" + (b - a) + "毫秒");
-//        } catch (Exception e) {
-//            Log.e(TAG, e.toString());
-//            return ERROR;
-//        }
-//        return OK;
-//    }
+    public void start(final PullToRefreshLayout pullToRefreshLayout) {
+        if (isFirstIn) {
+            dialog = new ProgressDialog(getContext());
+            dialog.setMessage("加载中...");
+            dialog.show();
+        }
+        Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                String result = getDatas();
+                subscriber.onNext(result);
+                subscriber.onCompleted();
+            }
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<String>() {
+                    @Override
+                    public void onCompleted() {
+                        if (dialog != null && dialog.isShowing()) {
+                            dialog.dismiss();
+                        }
+                        if (pullToRefreshLayout != null) {
+                            pullToRefreshLayout.refreshFinish(PullToRefreshLayout.SUCCEED);
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        if (dialog != null && dialog.isShowing()) {
+                            dialog.dismiss();
+                        }
+                        if (pullToRefreshLayout != null) {
+                            pullToRefreshLayout.refreshFinish(PullToRefreshLayout.FAIL);
+                        }
+                        Utils.makeText(SeasonHotFragment.this.getActivity(), "网络错误");
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+    }
+
+    private String getDatas() {
+        try {
+            long a = System.currentTimeMillis();
+            Connection connection;
+            Document doc;
+            if (isFirstIn) {
+                connection = Jsoup.connect(DEFAULT_MAIN_URL);
+            } else {
+                connection = Jsoup.connect(mNextPageUrl);
+            }
+            connection.header("User-Agent", USER_AGENT);
+            doc = connection.get();
+            //“椒麻鸡”和它对应的图片都在<div class="pic">中
+            Elements titleAndPics = doc.select("div.pic");
+            //原料在<p class="subcontent">中
+            Elements burdens = doc.select("p.subcontent");
+            //所需链接在<div class="detail">中的<a>标签里面
+            Elements detail = doc.select("div.detail");
+            for (int i = 0; i < titleAndPics.size(); i++) {
+                //使用Element.select(String selector)查找元素，使用Node.attr(String key)方法取得一个属性的值
+                String title = titleAndPics.get(i).select("a").attr("title");
+                String pic = titleAndPics.get(i).select("a").select("img").attr("data-src");
+                String url = detail.get(i).select("a").first().attr("href");
+                //对于一个元素中的文本，可以使用Element.text()方法
+                String burden = burdens.get(i).text();
+                Log.e(TAG, "tvTitle:" + title);
+                Log.e(TAG, "pic:" + pic);
+                Log.e(TAG, "url:" + url);
+                Log.e(TAG, "burden:" + burden);
+                list.add(title + "=====" + pic + "=====" + burden + "=====" + url);
+            }
+            Elements pageElements = doc.select("div.ui-page-inner");
+            Element nowPageElement = pageElements.select("a.now_page").get(0);
+            Element nextPageElement = nowPageElement.nextElementSibling();
+            mNextPageUrl = nextPageElement.attr("href");
+            isFirstIn = false;
+            long b = System.currentTimeMillis();
+            Log.e(TAG, "所用时间：" + (b - a) + "毫秒");
+        } catch (Exception e) {
+            Log.e(TAG, e.toString());
+            return ERROR;
+        }
+        return OK;
+    }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         unbinder.unbind();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (dialog != null) {
+            dialog.cancel();
+            dialog = null;
+        }
     }
 }
