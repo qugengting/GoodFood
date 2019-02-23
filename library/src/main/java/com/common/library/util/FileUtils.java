d69adac093fbac7718a24862af805a7dc66003cb
xuruibin <xuruibin@nistone.cn>
2019-02-23 17:44:48 +0800

    新增底部弹框控件ActionSheetDialog，使用new ActionSheetDialog(context).builder...

--- library/src/main/java/com/common/library/util/FileUtils.java
+++ library/src/main/java/com/common/library/util/FileUtils.java
@@ -3,13 +3,16 @@ package com.common.library.util;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Base64;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.text.DecimalFormat;
 
 /**
  * Created by xuruibin on 2018/5/11.
@@ -55,7 +58,7 @@ public class FileUtils {
         if (dotIndex < 0) {
             return type;
         }
-		/* 获取文件的后缀名 */
+        /* 获取文件的后缀名 */
         String end = fName.substring(dotIndex, fName.length()).toLowerCase();
         if ("".equals(end)) {
             return type;
@@ -70,68 +73,68 @@ public class FileUtils {
 
     private static final String[][] MIME_MapTable = {
             // {后缀名，MIME类型}
-            { ".3gp", "video/3gpp" },
-            { ".apk", "application/vnd.android.package-archive" },
-            { ".asf", "video/x-ms-asf" },
-            { ".avi", "video/x-msvideo" },
-            { ".bin", "application/octet-stream" },
-            { ".bmp", "image/bmp" },
-            { ".c", "text/plain" },
-            { ".class", "application/octet-stream" },
-            { ".conf", "text/plain" },
-            { ".cpp", "text/plain" },
-            { ".doc", "application/msword" },
-            { ".docx",
-                    "application/vnd.openxmlformats-officedocument.wordprocessingml.document" },
-            { ".xls", "application/vnd.ms-excel" },
-            { ".xlsx",
-                    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" },
-            { ".exe", "application/octet-stream" },
-            { ".gif", "image/gif" },
-            { ".gtar", "application/x-gtar" },
-            { ".gz", "application/x-gzip" },
-            { ".h", "text/plain" },
-            { ".htm", "text/html" },
-            { ".html", "text/html" },
-            { ".jar", "application/java-archive" },
-            { ".java", "text/plain" },
-            { ".jpeg", "image/jpeg" },
-            { ".jpg", "image/jpeg" },
-            { ".js", "application/x-javascript" },
-            { ".log", "text/plain" },
-            { ".m3u", "audio/x-mpegurl" },
-            { ".m4a", "audio/mp4a-latm" },
-            { ".m4b", "audio/mp4a-latm" },
-            { ".m4p", "audio/mp4a-latm" },
-            { ".m4u", "video/vnd.mpegurl" },
-            { ".m4v", "video/x-m4v" },
-            { ".mov", "video/quicktime" },
-            { ".mp2", "audio/x-mpeg" },
-            { ".mp3", "audio/x-mpeg" },
-            { ".mp4", "video/mp4" },
-            { ".mpc", "application/vnd.mpohun.certificate" },
-            { ".mpe", "video/mpeg" },
-            { ".mpeg", "video/mpeg" },
-            { ".mpg", "video/mpeg" },
-            { ".mpg4", "video/mp4" },
-            { ".mpga", "audio/mpeg" },
-            { ".msg", "application/vnd.ms-outlook" },
-            { ".ogg", "audio/ogg" },
-            { ".pdf", "application/pdf" },
-            { ".png", "image/png" },
-            { ".pps", "application/vnd.ms-powerpoint" },
-            { ".ppt", "application/vnd.ms-powerpoint" },
-            { ".pptx",
-                    "application/vnd.openxmlformats-officedocument.presentationml.presentation" },
-            { ".prop", "text/plain" }, { ".rc", "text/plain" },
-            { ".rmvb", "audio/x-pn-realaudio" }, { ".rtf", "application/rtf" },
-            { ".sh", "text/plain" }, { ".tar", "application/x-tar" },
-            { ".tgz", "application/x-compressed" }, { ".txt", "text/plain" },
-            { ".wav", "audio/x-wav" }, { ".wma", "audio/x-ms-wma" },
-            { ".wmv", "audio/x-ms-wmv" },
-            { ".wps", "application/vnd.ms-works" }, { ".xml", "text/plain" },
-            { ".z", "application/x-compress" },
-            { ".zip", "application/x-zip-compressed" }, { "", "*/*" }
+            {".3gp", "video/3gpp"},
+            {".apk", "application/vnd.android.package-archive"},
+            {".asf", "video/x-ms-asf"},
+            {".avi", "video/x-msvideo"},
+            {".bin", "application/octet-stream"},
+            {".bmp", "image/bmp"},
+            {".c", "text/plain"},
+            {".class", "application/octet-stream"},
+            {".conf", "text/plain"},
+            {".cpp", "text/plain"},
+            {".doc", "application/msword"},
+            {".docx",
+                    "application/vnd.openxmlformats-officedocument.wordprocessingml.document"},
+            {".xls", "application/vnd.ms-excel"},
+            {".xlsx",
+                    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+            {".exe", "application/octet-stream"},
+            {".gif", "image/gif"},
+            {".gtar", "application/x-gtar"},
+            {".gz", "application/x-gzip"},
+            {".h", "text/plain"},
+            {".htm", "text/html"},
+            {".html", "text/html"},
+            {".jar", "application/java-archive"},
+            {".java", "text/plain"},
+            {".jpeg", "image/jpeg"},
+            {".jpg", "image/jpeg"},
+            {".js", "application/x-javascript"},
+            {".log", "text/plain"},
+            {".m3u", "audio/x-mpegurl"},
+            {".m4a", "audio/mp4a-latm"},
+            {".m4b", "audio/mp4a-latm"},
+            {".m4p", "audio/mp4a-latm"},
+            {".m4u", "video/vnd.mpegurl"},
+            {".m4v", "video/x-m4v"},
+            {".mov", "video/quicktime"},
+            {".mp2", "audio/x-mpeg"},
+            {".mp3", "audio/x-mpeg"},
+            {".mp4", "video/mp4"},
+            {".mpc", "application/vnd.mpohun.certificate"},
+            {".mpe", "video/mpeg"},
+            {".mpeg", "video/mpeg"},
+            {".mpg", "video/mpeg"},
+            {".mpg4", "video/mp4"},
+            {".mpga", "audio/mpeg"},
+            {".msg", "application/vnd.ms-outlook"},
+            {".ogg", "audio/ogg"},
+            {".pdf", "application/pdf"},
+            {".png", "image/png"},
+            {".pps", "application/vnd.ms-powerpoint"},
+            {".ppt", "application/vnd.ms-powerpoint"},
+            {".pptx",
+                    "application/vnd.openxmlformats-officedocument.presentationml.presentation"},
+            {".prop", "text/plain"}, {".rc", "text/plain"},
+            {".rmvb", "audio/x-pn-realaudio"}, {".rtf", "application/rtf"},
+            {".sh", "text/plain"}, {".tar", "application/x-tar"},
+            {".tgz", "application/x-compressed"}, {".txt", "text/plain"},
+            {".wav", "audio/x-wav"}, {".wma", "audio/x-ms-wma"},
+            {".wmv", "audio/x-ms-wmv"},
+            {".wps", "application/vnd.ms-works"}, {".xml", "text/plain"},
+            {".z", "application/x-compress"},
+            {".zip", "application/x-zip-compressed"}, {"", "*/*"}
     };
 
     public static void openFile(Context context, File file) {
@@ -157,4 +160,173 @@ public class FileUtils {
         }
 
     }
+
+    /**
+     * @param path 文件所在的绝对路径加文件名
+     * @return
+     */
+    public static String fileBase64String(String path) {
+        try {
+            FileInputStream fis = new FileInputStream(path);//转换成输入流
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int count = 0;
+            while ((count = fis.read(buffer)) >= 0) {
+                baos.write(buffer, 0, count);//读取输入流并写入输出字节流中
+            }
+            fis.close();//关闭文件输入流
+            String uploadBuffer = new String(Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT));  //进行Base64编码
+            return uploadBuffer;
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+
+    private static final String TAG = FileUtils.class.getSimpleName();
+
+    public static final int SIZETYPE_B = 1;//获取文件大小单位为B的double值
+    public static final int SIZETYPE_KB = 2;//获取文件大小单位为KB的double值
+    public static final int SIZETYPE_MB = 3;//获取文件大小单位为MB的double值
+    public static final int SIZETYPE_GB = 4;//获取文件大小单位为GB的double值
+
+    /**
+     * 获取文件指定文件的指定单位的大小
+     *
+     * @param filePath 文件路径
+     * @param sizeType 获取大小的类型1为B、2为KB、3为MB、4为GB
+     * @return double值的大小
+     */
+    public static double getFileOrFilesSize(String filePath, int sizeType) {
+        File file = new File(filePath);
+        long blockSize = 0;
+        try {
+            if (file.isDirectory()) {
+                blockSize = getFileSizes(file);
+            } else {
+                blockSize = getFileSize(file);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, "获取文件大小失败!");
+        }
+        return FormetFileSize(blockSize, sizeType);
+    }
+
+    /**
+     * 调用此方法自动计算指定文件或指定文件夹的大小
+     *
+     * @param filePath 文件路径
+     * @return 计算好的带B、KB、MB、GB的字符串
+     */
+    public static String getAutoFileOrFilesSize(String filePath) {
+        File file = new File(filePath);
+        long blockSize = 0;
+        try {
+            if (file.isDirectory()) {
+                blockSize = getFileSizes(file);
+            } else {
+                blockSize = getFileSize(file);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, "获取文件大小失败!");
+        }
+        return FormetFileSize(blockSize);
+    }
+
+    /**
+     * 获取指定文件大小
+     *
+     * @param file
+     * @return
+     * @throws Exception
+     */
+    private static long getFileSize(File file) throws Exception {
+        long size = 0;
+        if (file.exists()) {
+            FileInputStream fis = null;
+            fis = new FileInputStream(file);
+            size = fis.available();
+        } else {
+            file.createNewFile();
+            Log.e(TAG, "获取文件大小不存在!");
+        }
+        return size;
+    }
+
+    /**
+     * 获取指定文件夹
+     *
+     * @param f
+     * @return
+     * @throws Exception
+     */
+    private static long getFileSizes(File f) throws Exception {
+        long size = 0;
+        File flist[] = f.listFiles();
+        for (int i = 0; i < flist.length; i++) {
+            if (flist[i].isDirectory()) {
+                size = size + getFileSizes(flist[i]);
+            } else {
+                size = size + getFileSize(flist[i]);
+            }
+        }
+        return size;
+    }
+
+    /**
+     * 转换文件大小
+     *
+     * @param fileS
+     * @return
+     */
+    private static String FormetFileSize(long fileS) {
+        DecimalFormat df = new DecimalFormat("#.00");
+        String fileSizeString = "";
+        String wrongSize = "0B";
+        if (fileS == 0) {
+            return wrongSize;
+        }
+        if (fileS < 1024) {
+            fileSizeString = df.format((double) fileS) + "B";
+        } else if (fileS < 1048576) {
+            fileSizeString = df.format((double) fileS / 1024) + "KB";
+        } else if (fileS < 1073741824) {
+            fileSizeString = df.format((double) fileS / 1048576) + "MB";
+        } else {
+            fileSizeString = df.format((double) fileS / 1073741824) + "GB";
+        }
+        return fileSizeString;
+    }
+
+    /**
+     * 转换文件大小,指定转换的类型
+     *
+     * @param fileS
+     * @param sizeType
+     * @return
+     */
+    private static double FormetFileSize(long fileS, int sizeType) {
+        DecimalFormat df = new DecimalFormat("#.00");
+        double fileSizeLong = 0;
+        switch (sizeType) {
+            case SIZETYPE_B:
+                fileSizeLong = Double.valueOf(df.format((double) fileS));
+                break;
+            case SIZETYPE_KB:
+                fileSizeLong = Double.valueOf(df.format((double) fileS / 1024));
+                break;
+            case SIZETYPE_MB:
+                fileSizeLong = Double.valueOf(df.format((double) fileS / 1048576));
+                break;
+            case SIZETYPE_GB:
+                fileSizeLong = Double.valueOf(df.format((double) fileS / 1073741824));
+                break;
+            default:
+                break;
+        }
+        return fileSizeLong;
+    }
+
 }
