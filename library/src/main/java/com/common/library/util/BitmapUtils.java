00933c77f444a7ad55dfdab77e51595542bfec3d
xuruibin <xuruibin@nistone.cn>
2019-09-23 09:56:07 +0800

    补充工具类

--- library/src/main/java/com/common/library/util/BitmapUtils.java
+++ library/src/main/java/com/common/library/util/BitmapUtils.java
@@ -17,11 +17,15 @@ import android.graphics.Shader;
 import android.graphics.drawable.Drawable;
 import android.media.ThumbnailUtils;
 import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Base64;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
@@ -45,7 +49,7 @@ public class BitmapUtils {
         int size = bitmap.getWidth() * bitmap.getHeight() * 4;
         ByteArrayOutputStream out = new ByteArrayOutputStream(size);
         try {
-            bitmap.compress(Bitmap.CompressFormat.PNG, quality, out);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, out);
             out.flush();
             out.close();
             return out.toByteArray();
@@ -245,6 +249,153 @@ public class BitmapUtils {
         Canvas canvas = new Canvas(bitmap); // 建立对应 bitmap 的画布
         drawable.setBounds(0, 0, width, height);
         drawable.draw(canvas); // 把 drawable 内容画到画布中
+        return bitmap;
+    }
+
+    /**
+     * 高级图片质量压缩
+     *
+     * @param bitmap  位图
+     * @param size 指定宽
+     */
+    public static Bitmap imageZoom(Bitmap bitmap, double size) {
+        // 将bitmap放至数组中，意在获得bitmap的大小（与实际读取的原文件要大）
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        // 格式、质量、输出流
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, baos);
+        byte[] b = baos.toByteArray();
+        Bitmap newBitmap = BitmapFactory.decodeByteArray(b, 0, b.length);
+        // 获取bitmap大小 是允许最大大小的多少倍
+        return scaleWithWH(newBitmap, size,
+                size * newBitmap.getHeight() / newBitmap.getWidth());
+    }
+
+    /***
+     * 图片缩放
+     *@param bitmap 位图
+     * @param w 新的宽度
+     * @param h 新的高度
+     * @return Bitmap
+     */
+    public static Bitmap scaleWithWH(Bitmap bitmap, double w, double h) {
+        if (w == 0 || h == 0 || bitmap == null) {
+            return bitmap;
+        } else {
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+
+            Matrix matrix = new Matrix();
+            float scaleWidth = (float) (w / width);
+            float scaleHeight = (float) (h / height);
+
+            matrix.postScale(scaleWidth, scaleHeight);
+            return Bitmap.createBitmap(bitmap, 0, 0, width, height,
+                    matrix, true);
+        }
+    }
+
+    /**
+     * bitmap保存到指定路径
+     *
+     * @param file 图片的绝对路径
+     * @param file 位图
+     * @return bitmap
+     */
+    public static boolean saveFile(String file, Bitmap bmp) {
+        if (TextUtils.isEmpty(file) || bmp == null) return false;
+
+        File f = new File(file);
+        if (f.exists()) {
+            f.delete();
+        } else {
+            File p = f.getParentFile();
+            if (!p.exists()) {
+                p.mkdirs();
+            }
+        }
+        try {
+            FileOutputStream out = new FileOutputStream(f);
+            bmp.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 回收一个未被回收的Bitmap
+     *
+     * @param bitmap
+     */
+    public static void doRecycledIfNot(Bitmap bitmap) {
+        if (!bitmap.isRecycled()) {
+            bitmap.recycle();
+        }
+    }
+
+    /**
+     * 将图片转换成Base64编码的字符串
+     */
+    public static String imageToBase64(String path) {
+        if (TextUtils.isEmpty(path)) {
+            return null;
+        }
+        InputStream is = null;
+        byte[] data = null;
+        String result = null;
+        try {
+            is = new FileInputStream(path);
+            //创建一个字符流大小的数组。
+            data = new byte[is.available()];
+            //写入数组
+            is.read(data);
+            //用默认的编码格式进行编码
+            result = android.util.Base64.encodeToString(data, android.util.Base64.DEFAULT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (null != is) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+        return result;
+    }
+
+    /**
+     * 将图片路径转Bitmap
+     *
+     * @return Bitmap
+     * @Param path 图片路径
+     */
+    public static Bitmap getBitmapPath(String path) {
+        return BitmapFactory.decodeFile(path);
+    }
+
+    /**
+     * String转换为bitmap
+     *
+     * @param string
+     * @return
+     */
+    public static Bitmap stringToBitmap(String string) {
+        // 将字符串转换成Bitmap类型
+        Bitmap bitmap = null;
+        try {
+            byte[] bitmapArray;
+            bitmapArray = Base64.decode(string, android.util.Base64.DEFAULT);
+            bitmap = BitmapFactory.decodeByteArray(bitmapArray, 0,
+                    bitmapArray.length);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return bitmap;
     }
 }
